d9e81fcf16890485527968fa27c286568b4d7a8b
pravin.behera <pravin.behera@hitechroboticsystemz.com>
2024-10-07 22:51:13 +0530

    modified

--- tut1_cpp_primitive_types.cpp
+++ tut1_cpp_primitive_types.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int main()
 {
+    // Character type
     char middle_initial {'J'};
     cout <<"My middle initial is "<< middle_initial<<std::endl;
 
@@ -11,6 +12,8 @@ int main()
     unsigned short int exam_score {55};
     cout << "My exam score was "<< exam_score << endl;
 
-    
+    int countries_represented {65};
+    cout << "There were " << countries_represented << " countries represented in my meeting" <<endl;
+
     return 0;
 }
