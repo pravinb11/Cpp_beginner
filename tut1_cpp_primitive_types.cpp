883b0a439bb00f3c2cced9b68997b637b684bb26
pravin.behera <pravin.behera@hitechroboticsystemz.com>
2024-10-07 22:43:36 +0530

    C++ Built-in Primitive Types

---
+++ tut1_cpp_primitive_types.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char middle_initial {'J'};
+    cout <<"My middle initial is "<< middle_initial<<std::endl;
+
+    // Integer types
+    unsigned short int exam_score {55};
+    cout << "My exam score was "<< exam_score << endl;
+
+    
+    return 0;
+}
