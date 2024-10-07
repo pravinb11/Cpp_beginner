c7c20b13d5bdd64abc476ce9880697141f7ea385
pravin.behera <pravin.behera@hitechroboticsystemz.com>
2024-10-07 23:25:11 +0530

    tut1 overflow error

--- tut1_cpp_primitive_types.cpp
+++ tut1_cpp_primitive_types.cpp
@@ -24,5 +24,33 @@ int main()
     long long distance_to_alpha_centauri {9'461'000'000'000};
     cout<< "The distance to alpha centauri is " << distance_to_alpha_centauri << " kilometers" <<endl;
 
+    // Floating point types
+    float car_payment { 401.23 };
+    cout << "My car payment is " << car_payment << endl;
+
+    double pi {3.14159};
+    cout << "PI is " << pi << endl;
+
+    long double large_amount {2.7e120};
+    cout << large_amount << " is a very big number " << endl;
+
+    // Boolean type
+    bool game_over {false};
+    cout << "The value of game_over is " << game_over << endl;
+
+    // Overflow example
+
+    short value1 {30000};
+    short value2 {1000};
+    short product {value1 * value2};
+
+    // Overflow error 
+    cout << "The product of " << value1 << " and "<< value2 << " is " << product << endl;
+
+    // Overflow error resolved
+    long product1 {value1 * value2};
+    cout << "The product of " << value1 << " and "<< value2 << " is " << product1 << endl;
+
+
     return 0;
 }
